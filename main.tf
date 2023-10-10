a356d3b5a80075295ea58c8382ecc5d6a40932b1
NielsG-it <56964270+NielsG-it@users.noreply.github.com>
2023-10-10 11:30:56 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,73 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-appserviceplan"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  kind                = "Linux"
+  reserved            = true
+  sku {
+    tier = "Basic"
+    size = "B1"
+  }
+}
+
+resource "azurerm_app_service" "example" {
+  name                = "example-webapp"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  app_service_plan_id = azurerm_app_service_plan.example.id
+
+  site_config {
+    dotnet_framework_version = "v5.0"
+    scm_type                 = "LocalGit"
+  }
+
+  app_settings = {
+    "DATABASE_URL" = azurerm_postgresql_server.example.fqdn
+  }
+}
+
+resource "azurerm_postgresql_server" "example" {
+  name                = "example-psqlserver"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  sku_name = "B_Gen5_1"
+
+  storage_mb        = 5120
+  backup_retention_days = 7
+  geo_redundant_backup_enabled = false
+  auto_grow_enabled = true
+
+  administrator_login          = "psqladminun"
+  administrator_login_password = var.PSQL_PASSWORD
+  version                      = "9.5"
+  ssl_enforcement_enabled      = true
+}
+
+resource "azurerm_postgresql_database" "example" {
+  name                = "exampledb"
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_postgresql_server.example.name
+  charset             = "UTF8"
+  collation           = "English_United States.1252"
+}
+
+# Output the Web App URL so you can visit the site once deployed
+output "webapp_url" {
+  value = "http://${azurerm_app_service.example.default_site_hostname}"
+}
+
+# Variables
+variable "PSQL_PASSWORD" {
+  description = "The password used for the PostgreSQL admin."
+  default     = ""
+}
